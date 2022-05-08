9f4e8469bb7b0a230bf9564b40561fed092898f1
Austin Putland <austinputland6464@gmail.com>
2022-05-08 16:21:29 -0600

    added IR Remote Mplab X project, assembly code

---
+++ IR_Remote.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=IR_Remote.X.production.obj
+CND_ARTIFACT_PATH_default=dist/default/production/IR_Remote.X.production.obj
